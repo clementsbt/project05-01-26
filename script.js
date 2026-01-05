8c368d8f85951caedefc8da5861cbf4222f312a7
clementsbt <clementsubtil2006@gmail.com>
2026-01-05 14:56:48 +0100

    add the reverse function

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const text = document.getElementById("animalText");
 const btnPerroquet = document.getElementById("btnPerroquet");
 const btnRenard = document.getElementById("btnRenard");
 
+const searchInput = document.getElementById("searchInput");
+const btnReverse = document.getElementById("btnReverse");
+const resultText = document.getElementById("resultText");
+
 btnPerroquet.addEventListener("click", () => {
   image.src = "image.jpg";
   text.textContent = "Perroquet";
@@ -12,4 +16,16 @@ btnPerroquet.addEventListener("click", () => {
 btnRenard.addEventListener("click", () => {
   image.src = "renard.jpg";
   text.textContent = "Renard";
+});
+
+function inverserOrdreMots(phrase) {
+  return phrase
+    .trim()
+    .split(/\s+/)
+    .reverse()
+    .join(" ");
+}
+
+btnReverse.addEventListener("click", () => {
+  resultText.textContent = inverserOrdreMots(searchInput.value);
 });
